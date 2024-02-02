ea54e5031c5fecc9c2ad04d3649c75712a0ee655
Pankaj Kumar <119965866+PankajBengaluru@users.noreply.github.com>
2024-02-02 16:13:37 +0530

    Create Book.vb

---
+++ Book.vb
@@ -0,0 +1,9 @@
+Public Class Book
+    Public Property BookID As Integer
+    Public Property Title As String
+    Public Property Author As String
+    Public Property AvailableCopies As Integer
+    Public Property TotalCopies As Integer
+
+    ' Constructors, getters, setters, and other methods
+End Class
